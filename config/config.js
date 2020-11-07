2d0d9ea8edc26d0fd6a4dda7d6015c0ded6c3784
RexterR <sumandas.workplace@gmail.com>
2020-11-07 11:06:33 +0530

    .env file added

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
-const keys = require("./keys");
-const MONGO_URI = process.env.MONGO_URI || keys.mongo_uri;
-const SENDGRID_KEY = process.env.SENDGRID_KEY || keys.sendgrid_key;
-const JWT_KEY = process.env.JWT_KEY || keys.jwt_key;
-const URL = process.env.URL || "localhost:3000";
+const dotenv = require("dotenv").config();
+const MONGO_URI = process.env.MONGO_URI;
+const SENDGRID_KEY = process.env.SENDGRID_KEY;
+const JWT_KEY = process.env.JWT_KEY;
+const URL = process.env.URL;
 module.exports = { MONGO_URI, SENDGRID_KEY, JWT_KEY, URL };
