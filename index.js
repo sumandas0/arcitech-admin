45c5a583a09fb81d9d46dce95a0c0aaaa4e9e27a
RexterR <sumandas.workplace@gmail.com>
2020-11-10 10:55:08 +0530

    Cors policy update

--- index.js
+++ index.js
@@ -15,7 +15,14 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use(flash());
 app.use(cors());
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(
   session({
     secret: "cookie_secret",
